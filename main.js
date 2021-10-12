82c2d3a4553601a466c5632148d9a5236c2a6dcb
Shweta270402011 <75731502+Shweta270402011@users.noreply.github.com>
2021-10-12 17:47:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+
+objects = [];
+status = "";
+
+
+function setup() {
+  canvas = createCanvas(380, 380);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(380,380);
+  video.hide();
+}
+
+
+function start() { 
+  objectDetector=ml5.objectDetector("cocossd ", modelLoaded);
+
+  document.getElementById("status").innerHTML="status: detecting object ";
+
+  object_name = document.getElementById("object_name").value;
+}
+
+
+
+function draw() {
+ image(video,0,0,380,380);
+
+     
+}
